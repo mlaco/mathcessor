e304e35056746d62bb01ef82cf180fe6be9571d9
Morgan Laco <morgan@codeschool.com>
2015-07-25 00:01:13 -0400

    Fix regex matching radicand

--- lib/mathcessor.rb
+++ lib/mathcessor.rb
@@ -6,7 +6,7 @@ module Mathcessor
   refine Array do
     def method_missing(method, *args, &block)
       method = method.to_s
-      match = /(square_root_of_([a-z]+_?)+)/.match(method)
+      match = /(square_root_of_((?:[a-z]+_)+[a-z]+))/.match(method)
       if match[1] == method && method.start_with?("square_root_of_")
         radicand = match[2].in_numbers
         sqrt = Math.sqrt(radicand).to_i
