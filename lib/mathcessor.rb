b7ba7b65b73551876cbe39b59a23cd6b07675b9e
Morgan Laco <morgan@codeschool.com>
2015-07-25 02:30:19 -0400

    Add cube root function, bump version

--- lib/mathcessor.rb
+++ lib/mathcessor.rb
@@ -3,22 +3,24 @@ require "numbers_in_words"
 require "numbers_in_words/duck_punch"
 
 module Mathcessor
-  METHOD_FAMILIES = {square_root_of_: Proc.new {|q| Math.sqrt(q).to_i}}
+  METHOD_FAMILIES = {
+    square_root_of: Proc.new {|q| Math.sqrt(q).to_i},
+    cube_root_of:   Proc.new {|q| (q ** (1.0/3)).to_i}
+  }
   refine Array do
     def method_missing(method, *args, &block)
       method = method.to_s
       METHOD_FAMILIES.each do |family_symbol, fun|
-        family = family_symbol.to_s
+        family = "#{family_symbol.to_s}_"
         regex = /(#{Regexp.quote(family)}((?:[a-z]+_)+[a-z]+))/
         match = regex.match(method)
-        if match[1] == method && method.start_with?(family)
+        if match && match[1] == method && method.start_with?(family)
           argument = match[2].in_numbers
           result = fun.call(argument)
           return self[result]
-        else
-          puts "nope"
         end
       end
+      false
     end
   end
 end
